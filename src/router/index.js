8b5b36453f6ee87628b3fa73755567b145df2daf
Noah <hanlun.song@gmail.com>
2020-03-30 01:08:43 -0400

    update readme; lint

--- src/router/index.js
+++ src/router/index.js
@@ -7,7 +7,8 @@ const routes = [
   {
     path: '/',
     name: 'Home',
-    component: () => import(/* webpackChunkName: "about" */ '../views/Home.vue')
+    component: () =>
+      import(/* webpackChunkName: "about" */ '../views/Home.vue'),
   },
   {
     path: '/game-of-life',
@@ -16,12 +17,12 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ '../views/GameOfLife.vue')
-  }
+      import(/* webpackChunkName: "about" */ '../views/GameOfLife.vue'),
+  },
 ]
 
 const router = new VueRouter({
-  routes
+  routes,
 })
 
 export default router
