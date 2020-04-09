742b720ab3687a8faee855b4e1e7bc834150b73c
Noah <hanlun.song@gmail.com>
2020-04-08 21:36:03 -0400

    use video texture

--- src/views/ColorBlockWebgl.vue
+++ src/views/ColorBlockWebgl.vue
@@ -10,6 +10,8 @@ import { mat4 } from 'gl-matrix'
 export default {
   mounted() {
     let cubeRotation = 0.0
+    // will set to true when video can be copied to texture
+    let copyVideo = false
 
     main(this)
 
@@ -33,38 +35,38 @@ export default {
       // Vertex shader program
 
       const vsSource = `
-    attribute vec4 aVertexPosition;
-    attribute vec3 aVertexNormal;
-    attribute vec2 aTextureCoord;
-    uniform mat4 uNormalMatrix;
-    uniform mat4 uModelViewMatrix;
-    uniform mat4 uProjectionMatrix;
-    varying highp vec2 vTextureCoord;
-    varying highp vec3 vLighting;
-    void main(void) {
-      gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
-      vTextureCoord = aTextureCoord;
-      // Apply lighting effect
-      highp vec3 ambientLight = vec3(0.3, 0.3, 0.3);
-      highp vec3 directionalLightColor = vec3(1, 1, 1);
-      highp vec3 directionalVector = normalize(vec3(0.85, 0.8, 0.75));
-      highp vec4 transformedNormal = uNormalMatrix * vec4(aVertexNormal, 1.0);
-      highp float directional = max(dot(transformedNormal.xyz, directionalVector), 0.0);
-      vLighting = ambientLight + (directionalLightColor * directional);
-    }
-  `
+        attribute vec4 aVertexPosition;
+        attribute vec3 aVertexNormal;
+        attribute vec2 aTextureCoord;
+        uniform mat4 uNormalMatrix;
+        uniform mat4 uModelViewMatrix;
+        uniform mat4 uProjectionMatrix;
+        varying highp vec2 vTextureCoord;
+        varying highp vec3 vLighting;
+        void main(void) {
+          gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
+          vTextureCoord = aTextureCoord;
+          // Apply lighting effect
+          highp vec3 ambientLight = vec3(0.3, 0.3, 0.3);
+          highp vec3 directionalLightColor = vec3(1, 1, 1);
+          highp vec3 directionalVector = normalize(vec3(0.85, 0.8, 0.75));
+          highp vec4 transformedNormal = uNormalMatrix * vec4(aVertexNormal, 1.0);
+          highp float directional = max(dot(transformedNormal.xyz, directionalVector), 0.0);
+          vLighting = ambientLight + (directionalLightColor * directional);
+        }
+      `
 
       // Fragment shader program
 
       const fsSource = `
-    varying highp vec2 vTextureCoord;
-    varying highp vec3 vLighting;
-    uniform sampler2D uSampler;
-    void main(void) {
-      highp vec4 texelColor = texture2D(uSampler, vTextureCoord);
-      gl_FragColor = vec4(texelColor.rgb * vLighting, texelColor.a);
-    }
-  `
+        varying highp vec2 vTextureCoord;
+        varying highp vec3 vLighting;
+        uniform sampler2D uSampler;
+        void main(void) {
+          highp vec4 texelColor = texture2D(uSampler, vTextureCoord);
+          gl_FragColor = vec4(texelColor.rgb * vLighting, texelColor.a);
+        }
+      `
 
       // Initialize a shader program; this is where all the lighting
       // for the vertices and so forth is established.
@@ -102,12 +104,11 @@ export default {
       // objects we'll be drawing.
       const buffers = initBuffers(gl)
 
-      const texture = loadTexture(
-        gl,
-        'https://images.pexels.com/photos/1509534/pexels-photo-1509534.jpeg',
-      )
+      const texture = initTexture(gl)
 
-      var then = 0
+      const video = setupVideo('./video/window.mp4')
+
+      let then = 0
 
       // Draw the scene repeatedly
       function render(now) {
@@ -115,11 +116,58 @@ export default {
         const deltaTime = now - then
         then = now
 
+        if (copyVideo) {
+          updateTexture(gl, texture, video)
+        }
+
         drawScene(gl, programInfo, buffers, texture, deltaTime)
 
         requestAnimationFrame(render)
       }
       requestAnimationFrame(render)
+    }
+
+    function setupVideo(url) {
+      const video = document.createElement('video')
+
+      let playing = false
+      let timeupdate = false
+
+      video.autoplay = true
+      video.muted = true
+      video.loop = true
+
+      // Waiting for these 2 events ensures
+      // there is data in the video
+
+      video.addEventListener(
+        'playing',
+        function() {
+          playing = true
+          checkReady()
+        },
+        true,
+      )
+
+      video.addEventListener(
+        'timeupdate',
+        function() {
+          timeupdate = true
+          checkReady()
+        },
+        true,
+      )
+
+      video.src = url
+      video.play()
+
+      function checkReady() {
+        if (playing && timeupdate) {
+          copyVideo = true
+        }
+      }
+
+      return video
     }
 
     //
@@ -467,18 +515,16 @@ export default {
     }
 
     //
-    // Initialize a texture and load an image.
-    // When the image finished loading copy it into the texture.
+    // Initialize a texture.
     //
-    function loadTexture(gl, url) {
+    function initTexture(gl) {
       const texture = gl.createTexture()
       gl.bindTexture(gl.TEXTURE_2D, texture)
 
-      // Because images have to be download over the internet
-      // they might take a moment until they are ready.
-      // Until then put a single pixel in the texture so we can
-      // use it immediately. When the image has finished downloading
-      // we'll update the texture with the contents of the image.
+      // Because video havs to be download over the internet
+      // they might take a moment until it's ready so
+      // put a single pixel in the texture so we can
+      // use it immediately.
       const level = 0
       const internalFormat = gl.RGBA
       const width = 1
@@ -499,40 +545,32 @@ export default {
         pixel,
       )
 
-      const image = new Image()
-      image.crossOrigin = 'anonymous'
-      image.onload = function() {
-        gl.bindTexture(gl.TEXTURE_2D, texture)
-        gl.texImage2D(
-          gl.TEXTURE_2D,
-          level,
-          internalFormat,
-          srcFormat,
-          srcType,
-          image,
-        )
-
-        // WebGL1 has different requirements for power of 2 images
-        // vs non power of 2 images so check if the image is a
-        // power of 2 in both dimensions.
-        if (isPowerOf2(image.width) && isPowerOf2(image.height)) {
-          // Yes, it's a power of 2. Generate mips.
-          gl.generateMipmap(gl.TEXTURE_2D)
-        } else {
-          // No, it's not a power of 2. Turn of mips and set
-          // wrapping to clamp to edge
-          gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE)
-          gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE)
-          gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR)
-        }
-      }
-      image.src = url
+      // Turn off mips and set  wrapping to clamp to edge so it
+      // will work regardless of the dimensions of the video.
+      gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE)
+      gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE)
+      gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR)
 
       return texture
     }
 
-    function isPowerOf2(value) {
-      return (value & (value - 1)) == 0
+    //
+    // copy the video texture
+    //
+    function updateTexture(gl, texture, video) {
+      const level = 0
+      const internalFormat = gl.RGBA
+      const srcFormat = gl.RGBA
+      const srcType = gl.UNSIGNED_BYTE
+      gl.bindTexture(gl.TEXTURE_2D, texture)
+      gl.texImage2D(
+        gl.TEXTURE_2D,
+        level,
+        internalFormat,
+        srcFormat,
+        srcType,
+        video,
+      )
     }
 
     //
